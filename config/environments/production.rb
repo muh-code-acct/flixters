4c72e5189306bddff3ecec23f51f6e27b04d2cb1
Jake <skellytech.jg@gmail.com>
2019-10-11 13:42:47 -0500

    lesson 11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
+
+  # Devise
+  config.action_mailer.default_url_options = { host: 'flixter-j-g.herokuapp.com' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
