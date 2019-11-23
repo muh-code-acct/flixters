8d8f5e810ccaeb1cb994b827cf06b46a1a493a04
Jake <skellytech.jg@gmail.com>
2019-11-23 14:02:42 -0600

    lesson 39

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :dashboard, only: [:show]
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'careers', to: 'static_pages#careers'
+  get 'team', to: 'static_pages#team'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
